1d097c205f0de642fff2a58f09e0247aae5da138
wildanwm <wld822@gmail.com>
2023-04-13 15:03:08 +0700

    delete unacessary

--- motor_n_encoder/control.ino
+++ motor_n_encoder/control.ino
@@ -18,6 +18,6 @@ float mapF(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-int ang2pwm(float ang) {
-  return map(ang, -maxV, maxV, -maxpwm, maxpwm);
-}
+//int ang2pwm(float ang) {
+//  return map(ang, -maxV, maxV, -maxpwm, maxpwm);
+//}
